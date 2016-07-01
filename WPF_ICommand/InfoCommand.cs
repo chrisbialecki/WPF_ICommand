b548609186c0fcc4a2c54e35264fd31464af9a3d
Krzysztof Bialecki <chrisbialecki@outlook.ie>
2016-07-01 17:52:04 +0100

    go

---
+++ WPF_ICommand/InfoCommand.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace WPF_ICommand
+{
+    public class InfoCommand : ICommand
+    {
+        public void Execute(object parameter)
+        {
+            MessageBox.Show("hello world");
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public event EventHandler CanExecuteChanged;
+    }
+}
